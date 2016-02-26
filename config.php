af7533ac516f89f1b78045428bdc4f5656651888
zzanette <alysson.zanette@hotmail.com>
2016-02-26 14:55:19 -0300

    Adicionando setup postgres

--- config.php
+++ config.php
@@ -3,19 +3,19 @@
 /* DATABASE CONFIGURATION */
 
 /** Name of Database */
-define('DB_NAME', '');
+define('DB_NAME', 'evidence');
 
 /** User of Database */
-define('DB_USER', '');
+define('DB_USER', 'postgres');
 
 /** Password of Database */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'evidence');
 
 /** hostname of database */
-define('DB_HOST', '');
+define('DB_HOST', 'localhost');
 
 /*Database port*/
-define('DB_PORT', '');
+define('DB_PORT', '5432');
 
 
 ?>
