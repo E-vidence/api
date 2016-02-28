62b6fb8a96de8285ba27a83c0689ae07d712f5bd
zzanette <alysson.zanette@hotmail.com>
2016-02-28 16:29:39 -0300

    Implementando feature User

--- apps/simple/routers/simple.default.router.php
+++ apps/simple/routers/simple.default.router.php
@@ -2,3 +2,7 @@
 // default index action, GET /
 $app->get('/', 'Simple\Controller\IndexController:actionIndex')
     ->name('get-homepage');
+
+// get users action, GET /users
+$app->get('/users', 'Simple\Controller\UserController:actionGetUsers')
+    ->name('get-user-list');
